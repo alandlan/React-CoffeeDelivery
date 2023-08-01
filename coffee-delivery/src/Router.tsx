41423445b8f8a4274a2378bb4da61e0b2a7f87fa
alandlan <alanhorcrux@gmail.com>
2023-08-01 18:45:48 -0300

    criando component header e footer

--- coffee-delivery/src/Router.tsx
+++ coffee-delivery/src/Router.tsx
@@ -3,15 +3,18 @@ import { Home } from './pages/home';
 import { Success } from './pages/success';
 import { Checkout } from './pages/checkout';
 import { CheckoutFilled } from './pages/checkout-filled';
+import { DefaultLayout } from './layout/DefaultLayout';
 
 export function Router(){
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/success" element={<Success />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/checkout-filled" element={<CheckoutFilled />} />
-            <Route path="*" element={<h1>Not Found</h1>} />
+            <Route path="/" element={ <DefaultLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/success" element={<Success />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/checkout-filled" element={<CheckoutFilled />} />
+                <Route path="*" element={<h1>Not Found</h1>} />
+            </Route>
         </Routes>
     )
 }
