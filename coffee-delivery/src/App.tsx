ad743ad90780eb7d78aa9770fa7a9bb4de57f807
alandlan <alanhorcrux@gmail.com>
2023-08-01 18:23:51 -0300

    add global style

--- coffee-delivery/src/App.tsx
+++ coffee-delivery/src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
+import { GlobalStyle } from "./styles/global.styles";
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <p>Ola</p>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
---
+++ coffee-delivery/src/styles/theme/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+    white: '#FFFFFF',
+
+    'product-yellow-dark': '#C47F17',
+    'product-yellow-light': '#F1E9C9',
+    'product-yellow': '#DBAC2C',
+
+    'product-purple-dark': '#4B2995',
+    'product-purple-light': '#EBE5F9',
+    'product-purple': '#8047F8',
+
+    'base-title': '#272221',
+    'base-text': '#574F4D',
+    'base-subtitle': '#403937',
+    'base-label': '#8D8686',
+    'base-hover': '#D7D5D5',
+
+    'base-button': '#E6E5E5',
+    'base-input': '#EDEDED',
+    'base-card': '#F3F2F2',
+    'background': '#FAFAFA',
+}
