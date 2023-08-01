9e7a7c093e8c5d1545b7f06c25254c7ce07cff17
alandlan <alanhorcrux@gmail.com>
2023-08-01 19:23:47 -0300

    config eslint e prettier

--- coffee-delivery/src/components/Header.tsx
+++ coffee-delivery/src/components/Header.tsx
@@ -1,15 +1,14 @@
-
 import logoIgnite from '../assets/logo.svg';
 
 export function Header() {
-    return (
-            <div>
-                <div>
-                    <img src={logoIgnite} alt="logo" />
-                </div>
-                <div>
-                    <h1>COFFEE DELIVERY</h1>
-                </div>
-            </div>
-    )
+	return (
+		<div>
+			<div>
+				<img src={logoIgnite} alt="logo" />
+			</div>
+			<div>
+				<h1>COFFEE DELIVERY</h1>
+			</div>
+		</div>
+	);
 }
