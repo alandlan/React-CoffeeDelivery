ab70727a867f582a8ebfa39cd06f3b170aa094b6
alandlan <alanhorcrux@gmail.com>
2023-08-23 15:28:54 -0300

    ajust layout

--- coffee-delivery/src/Router.tsx
+++ coffee-delivery/src/Router.tsx
@@ -1,20 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home';
-import { Success } from './pages/success';
-import { Checkout } from './pages/checkout';
-import { CheckoutFilled } from './pages/checkout-filled';
+import { Success } from './pages/Order/Success';
+import { Checkout } from './pages/Order/Checkout';
+import { CheckoutFilled } from './pages/Order/Checkout-filled';
 import { DefaultLayout } from './layout/DefaultLayout';
 
-export function Router(){
-    return (
-        <Routes>
-            <Route path="/" element={ <DefaultLayout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/success" element={<Success />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/checkout-filled" element={<CheckoutFilled />} />
-                <Route path="*" element={<h1>Not Found</h1>} />
-            </Route>
-        </Routes>
-    )
+export function Router() {
+	return (
+		<Routes>
+			<Route path="/" element={<DefaultLayout />}>
+				<Route path="/" element={<Home />} />
+				<Route path="/success" element={<Success />} />
+				<Route path="/checkout" element={<Checkout />} />
+				<Route path="/checkout-filled" element={<CheckoutFilled />} />
+				<Route path="*" element={<h1>Not Found</h1>} />
+			</Route>
+		</Routes>
+	);
 }
