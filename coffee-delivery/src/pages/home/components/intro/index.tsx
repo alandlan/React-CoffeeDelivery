bf3904f73fc76327d6bdce5a719767d3570f945f
alandlan <alanhorcrux@gmail.com>
2023-08-24 16:53:51 -0300

    finalizando intro

--- coffee-delivery/src/pages/home/components/intro/index.tsx
+++ coffee-delivery/src/pages/home/components/intro/index.tsx
@@ -1,8 +1,18 @@
-import { IntroContainer, IntroContent, IntroTitle } from './styles';
+import {
+	BenefitsContainer,
+	IntroContainer,
+	IntroContent,
+	IntroTitle,
+} from './styles';
 import introImg from '../../../../assets/intro-img.png';
 import { RegularText } from '../../../../components/typography';
+import { InfoWithIcon } from '../../../../components/infowithicon';
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react';
+import { useTheme } from 'styled-components';
 
 export function Intro() {
+	const { colors } = useTheme();
+
 	return (
 		<IntroContainer>
 			<IntroContent className="container">
@@ -16,6 +26,29 @@ export function Intro() {
 							estiver, a qualquer hora
 						</RegularText>
 					</section>
+
+					<BenefitsContainer>
+						<InfoWithIcon
+							iconBg={colors['brand-yellow-dark']}
+							text="Compra simples e segura"
+							icon={<ShoppingCart weight="fill" />}
+						/>
+						<InfoWithIcon
+							iconBg={colors['base-text']}
+							text="Embalagem mantém o café fresco"
+							icon={<Package weight="fill" />}
+						/>
+						<InfoWithIcon
+							iconBg={colors['brand-yellow']}
+							text="Entrega rápida"
+							icon={<Timer weight="fill" />}
+						/>
+						<InfoWithIcon
+							iconBg={colors['brand-purple']}
+							text="O café chega quentinho"
+							icon={<Coffee weight="fill" />}
+						/>
+					</BenefitsContainer>
 				</div>
 
 				<img src={introImg} alt="Coffee" />
